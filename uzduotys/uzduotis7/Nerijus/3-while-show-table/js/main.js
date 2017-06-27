c548aaff8efd6fcd12b1f6356a47cbcf7a211c6c
nerijus123 <nerijuscustrans@gmail.com>
2017-06-27 16:10:32 +0300

    nauji failai

--- uzduotys/uzduotis7/Nerijus/3-while-show-table/js/main.js
+++ uzduotys/uzduotis7/Nerijus/3-while-show-table/js/main.js
@@ -10,28 +10,32 @@ console.log("labas");
 // |
 // -----------------------------------------
 
-// piestiEilute(45);
-// nupiestiLentele("nr", "name", "last Name");
-// piestiEilute(45);
-// piestiStulpeli(5);
+piestiEilute(45);
+nupiestiLentele("nr", "name", "last Name");
+piestiEilute(45);
+piestiStulpeli(5);
 piestiEilute = 0;
 
 function print(piestiEilute) {
   console.log("-");
 }
-// tekstas = "-";
-// while (piestiEilute < 40) {
-//   tekstas = tekstas + "-";
-//   piestiEilute = piestiEilute + 1;
-// }
-// console.log(tekstas);
+
 
 function piestiEilute1(x) {
   tekstas = "-";
   while (piestiEilute < x) {
     tekstas = tekstas + "-";
-    piestiEilute = piestiEilute + 1;
+    piestiEilute = piestiEilute + 1; // IDEA: piestiEilute++;
   }
   console.log(tekstas);
 }
-piestiEilute1(20);
+// piestiEilute1(20);
+function piestiStulpeli(stulpAukstis) {
+i = 0;
+tekstas = "|";
+while (i < stulpAukstis) {
+  console.log("|");
+  tekstas = tekstas + " ";
+  i++;
+}
+}
