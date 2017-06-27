499db78bead866f983725da3f105873eff317744
tautvydascoding <tautvydascoding@gmail.com>
2017-06-27 15:25:58 +0300

    uzduotis 7-1

--- uzduotys/uzduotis7/tautvydas/3/js/main.js
+++ uzduotys/uzduotis7/tautvydas/3/js/main.js
@@ -2,9 +2,9 @@ console.log("labas");
 
 
 
-// ----------------------------------------
+// ------------------------------------------------------------
 // |    nr     |     Name     |     lName    |
-// ----------------------------------------
+// ------------------------------------------------------------
 // |
 // |
 // |
@@ -20,8 +20,31 @@ console.log("labas");
 //  2) piestiStulpeli(x)    f-ja kioje while ciklas nupiesia stulpeli
 // 3) nupiestiLentelesStulpeliuPavadinimus(firstColName, secondColName, lastColName) Nereik while!
 
-piestiEilute(45);
-nupiestiLentelesStulpeliuPavadinimus("nr", "Name", "Last Name");
-piestiEilute(45);
+piestiEilute(60);
+nupiestiLentelesStulpeliuPavadinimus("      nr      ", "     Name     ", "    Last Name    ");
+piestiEilute(60);
 piestiStulpeli(5);
 piestiEilute(45);
+
+function piestiEilute(eilIlgis) {
+  var i = 0;
+  tekstas = "";
+  while (i < eilIlgis) {
+    tekstas = tekstas + "-";  // tekstas += "-";
+    i++;       // i = i + 1;
+  }
+  console.log( tekstas );
+}
+
+function piestiStulpeli(stulpAukstis) {
+  i = 0;
+  tekstas = "|";
+  while (i < stulpAukstis) {
+      tekstas = tekstas + " ";
+      console.log(tekstas);
+      i++;
+  }
+}
+function nupiestiLentelesStulpeliuPavadinimus(x1, x2, x3) {
+  console.log("|", x1, "|", x2, "|", x3, "|");
+}
