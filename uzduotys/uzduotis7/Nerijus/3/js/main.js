dcc3dde12b97f5da32a3b343d1b8530ca718310e
nerijus123 <nerijuscustrans@gmail.com>
2017-06-27 14:24:03 +0300

    nauji failai

---
+++ uzduotys/uzduotis7/Nerijus/3/js/main.js
@@ -0,0 +1,28 @@
+console.log("labas");
+
+// ----------------------------------------
+// | nr       |   name    |     lname     |
+// ----------------------------------------
+// |
+// |
+// |
+// |
+// |
+// -----------------------------------------
+
+// piestiEilute(45);
+// nupiestiLentele("nr", "name", "last Name");
+// piestiEilute(45);
+// piestiStulpeli(5);
+piestiEilute = 0;
+function print (piestiEilute) {
+console.log("-");
+}
+while (piestiEilute < 40) {
+
+  piestiEilute = piestiEilute + 1;
+  tekstas = "-";
+  tekstas = tekstas + "-";
+  console.log(tekstas);
+
+}
