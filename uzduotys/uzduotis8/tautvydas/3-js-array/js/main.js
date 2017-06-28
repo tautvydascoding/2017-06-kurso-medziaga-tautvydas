a56412a3358eccc32192741d2662c62b0e190f7d
tautvydascoding <tautvydascoding@gmail.com>
2017-06-28 16:25:06 +0300

    uzduotis 8-1

--- uzduotys/uzduotis8/tautvydas/3-js-array/js/main.js
+++ uzduotys/uzduotis8/tautvydas/3-js-array/js/main.js
@@ -20,10 +20,28 @@ delete masinosBagazine[3]; // nelabai naudoti, nes skalvisu liks, o ti kturini i
 masinosBagazine.splice(2,1);
 
 
-// 4. sukurti masyva (50 ilgumo) ir uzpildyti skaiciu 0 (kiekvienas stalcius turi but lygus "0");
+// 4. sukurti masyva (50 ilgumo) naudojant FOR ir uzpildyti skaiciu 0 (kiekvienas stalcius turi but lygus "0");
 // 5.  sukurti masyva naudojant WHILE (50 ilgumo) ir uzpildyti skaiciu 1;
 
 
+// 4. SPENDIMAS
+sarasas = new Array(50) ;
+sarasas.fill(0);
+
+sarasas = [];
+for (  i = 0; i < 50; i++) {
+   sarasas[i] = 0;
+}
+console.log("FOR sarasas: ", sarasas); // sarasas.toString; sarasas.join(" ; ");
+
+// 5. SPENDIMAS
+i = 0;
+sarasas2 = [];
+while ( i < 50) {
+  sarasas2[i] = 1;
+  i++;
+}
+console.log("WHILE sarasas: ", sarasas2); // sarasas2.toString; sarasas2.join(" ; ");
 
 
- //
+ // ND: kas antra ^ masyvo elementa pakeisti i "3"
