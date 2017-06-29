6f910c5b3bc1e48581941779d89d15c6fbbf1a0a
tautvydascoding <tautvydascoding@gmail.com>
2017-06-29 16:37:28 +0300

    uzduotis 9-1

--- uzduotys/uzduotis9/3-js/js/main.js
+++ uzduotys/uzduotis9/3-js/js/main.js
@@ -6,10 +6,13 @@ console.log("labas");
 // uzduotis:--------------------------
 // 2) parasyti funkcija, kuriai davus iekoma zodi , ji suranda jo vieta masyve (skalciuas numeri) ir si   numeri
 // uzduots:----------------------------
-// rasti pavarde masyve esancio  zmogaus vardu "Freida" (pirmojo)
+// 3) rasti pavarde masyve esancio  zmogaus vardu "Freida" (pirmojo)
 // rasti visu zmoniu vardu "Rico" pavardes
 
 var names = ["Enriqueta", "Sybil", "Piper", "Anh", "Carmelo", "Regan", "Synthia", "Newton", "Norbert", "Krystyna", "Fidelia", "Christoper", "Lewis", "Jeromy", "Joy", "Lorri", "Owen", "Rosenda", "Devora", "Treva", "Leanora", "Meghann", "Jacqueline", "Bunny", "Tenisha", "Rico", "Clementina", "Samella", "Rico", "Sandi", "Efrain", "Tena", "Vivan", "Hiedi", "Naida", "Evette", "Shane", "Freida", "Marielle", "Wynona", "Cheree", "Gaston", "Aja", "Timika", "Jude", "Griselda", "Luise", "Rico", "Minh", "Warren"];
+
+var lastNames = ["Mcdowell", "Gates", "Mccall", "Cisneros", "Hancock", "Gaines", "Juarez", "Nolan", "Barajas", "Ware", "Orr", "Bell", "Donovan", "Rojas", "Stevenson", "Long", "Hays", "Gibson", "Meyer", "Sims", "Mcintosh", "Craig", "Haney", "Cunningham", "Hunt", "Montgomery", "Spears", "Cooke", "Gregory", "Mcknight", "Fernandez", "Hendrix", "Patton", "Bond", "Skinner", "Randolph", "Montes", "Guerra", "Bowen", "Potts", "Dyer", "Riley", "Rodgers", "Schroeder", "Ferguson", "Garrett", "Rush", "Moon", "Whitney", "Mcdaniel"];
+
 
 // 1A)
 for ( i = 0; i < names.length; i++) {
@@ -39,17 +42,40 @@ for ( x in names) {
   }
 }
 // kai atlikom paieska, pasitikrinti ar radome
-if ( arRadome) {
+if ( arRadome) {  //arRadome === true        !arRadome => arRadome == false
   console.log("Radome Riko");
 } else {
-  console.log();
+  console.log("Nieko neradome...Bnadykite kita fraze");
 }
 
 
+// 2) parasyti funkcija, kuriai davus iekoma zodi , ji suranda jo vieta masyve (skalciuas numeri) ir si   numeri
+
+console.log("----------Ieskau vardo pozicijos -------");
+
+function rastiVardoPozicija (ieskomasVardas) {
+  for (y in names) { // begam per masyva, nuo pradzio iki galo
+    // tikrinu ar masyvo stalciaus vardas yra ieskomas zodis
+    if (names[y] === ieskomasVardas) {
+      return y;   // graziname - y stalciaus numeris
+    }
+  }
+}
+vietaMasyve = rastiVardoPozicija("Joy");
+console.log("ieskomoo zodzio vieta: " + vietaMasyve);
 
 
+// 3) rasti pavarde masyve esancio  zmogaus vardu "Freida" (pirmojo)
+console.log("----------Ieskau zmogaus pavardes pagal uzduota varda  -------");
 
+numerisMasyve = rastiVardoPozicija("Sybil");
+console.log("Sybil pavarde: " + lastNames[numerisMasyve]);
 
+// funkcija kuri gauna pavarde pagal uzduota varda
+function getLastName(name) {
+  numerisMasyve = rastiVardoPozicija(name);
+  return lastNames[numerisMasyve];
+}
 
 
 //
