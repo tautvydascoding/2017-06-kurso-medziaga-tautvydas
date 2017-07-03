2e876c99777bbed9cf797530e9183d7f2256816c
zhirgis <katkeviciuz@gmail.com>
2017-07-03 12:24:12 +0300

    jquery

---
+++ uzduotys/uzduotis11/zygis/2/js/main.js
@@ -0,0 +1,19 @@
+//funkcija kuri pati save issikviecia
+
+
+(function(){
+    console.log("iskvieciu save");
+}) (); //-va sitoj vietoj
+
+
+
+
+//nenaudojant anonimines funkcijos
+
+$('p').click(funkcijosPav(this));
+
+function funkcijosPav(xx){
+    $(xx).hide(); //paslepia visus xx
+}
+
+
