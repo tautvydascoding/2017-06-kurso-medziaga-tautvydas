ca12e7615965a4d75f05cedede235ad638a79ece
Derongalatis <deronbderon@gmail.com>
2017-07-04 12:03:36 +0300

    opa

--- uzduotys/MAMP/uzduotis12/Indrius/testas.php
+++ uzduotys/MAMP/uzduotis12/Indrius/testas.php
@@ -6,8 +6,56 @@
 echo "Labas rytas";
 
 
-$vardas = "Tomas"
+$vardas = "Tomas";
+
+echo " mano vardas $vardas <br>";
+echo " Kieno vardas $vardas <br>";
+
+
+define("DATABASENAME", "DB_valio");
+define("NUMBEROFARTICLES",6);
+
+echo "Musu duomenu baze " . DATABASENAME . " atvaizduoja " . NUMBEROFARTICLES . " straipsnius ";
+
+$lname ="TOmka";
+if (strlen($lname) <= 20) {
+  echo "Pavarde trumpesne nei 20 simboliu <br>";
+  if ($lname === "Tomka"){
+    echo "Pavarde Tomka";
+  } elseif ($lname === "TOmka") {
+  echo "Pavarde TOmka";
+  }
+} else {
+  echo "Pavarde ilgesne nei 20 simboliu <br>";
+}
+
+$i = 0;
+do {
+
+  $i++;
+} while ($i <=10);
+
+// FOREACH-darbui su masyvais //
+$names = [];
+foreach ($names as $name) {
+  echo $name;
+
+
+
+
+
+
+
+  
+}
+
+
+
+
+
+
+
+
+
 
 ?>
-
-cia paprastas tekstas
