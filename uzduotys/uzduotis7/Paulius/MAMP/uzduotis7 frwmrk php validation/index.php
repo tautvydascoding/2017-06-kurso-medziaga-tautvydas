d7b97d462cc631b1abdc6890f7b27efceb974f8f
splin911 <paulius.pastauka@gmail.com>
2017-07-05 14:34:25 +0300

    hey

---
+++ uzduotys/uzduotis7/Paulius/MAMP/uzduotis7 frwmrk php validation/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+
+<body>
+<!--<form action="email.php" method="get">-->
+<input class="half-width first-input" type="text" name="firstname" placeholder="First name"><br><br>
+<input class="half-width" type="text" name="question" placeholder="Kas neaišku?!?"><br><br> 
+<input class="full-width" type="email" name="email" placeholder="Your email address"><br><br>
+<input type="submit" value="Send">
+</form>
+    <script src="main.js"></script>
+</body>
+
+</html>
