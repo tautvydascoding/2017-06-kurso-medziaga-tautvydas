b69d00d033ef5a2aa1802496a73b4b171b48bbdd
tautvydascoding <tautvydascoding@gmail.com>
2017-07-10 10:52:33 +0300

    paskaita 14

--- uzduotys/MAMP/uzduotis14/uzduotis14-php-extends.php
+++ uzduotys/MAMP/uzduotis14/uzduotis14-php-extends.php
@@ -1,19 +1,24 @@
 // =====================extends=====================
- 
+
  // uzduotis: sukurti klases "Gyvunas", "Tipas" - kuri paveldi Gyvunas, "Liutas" - kuri paveldi Tipas
- Gyvunas:
-               public $alive = true;
-              public $positionX;
-              public $positionY;
- Tipas:        
-    public $rusis = "kate";
-    public $minta = "mesa";
-Liutas: 
-                public $kailis = true;
-                public $spalva = "#a34baa";
+ // Gyvunas:
+ //    public $alive = true;
+ //    public $positionX;
+ //    public $positionY;
+ // Tipas:
+ //    public $rusis = "kate";
+ //    public $minta = "mesa";
+ // Liutas:
+ //    public $kailis = true;
+ //    public $spalva = "#a34baa";
+ //    private $amzius = 5;
+
+ // uzduotis 0: susikurti 3 Obj. klasas Liutas
   // uzduotis 1: pakeisti visu triju liutu spalvas (ir pakeitimus atspausdinti)
-         // uzduotis2: Liuto klasei susikurti privatu kintamaji "Name". ir uskurti 2 funkcijas jo reiksmei pakeisti ir paimti "setName($x)", "getName()".
-              // uzduotis 3:
-              // 1) sukurti dvi klases: User, Admin
-              // 2) User klase turi kintamuosius : username, password, rights ir funkcija Login($name, $pass)
-              // 3) Admin klase  funkcijas:  changeUserName($nm),  changeRights($rgt)
+
+   // uzduotis2: A) Liuto klasei susikurti PRIVATU kintamaji "name". ir uskurti 2 funkcijas jo reiksmei pakeisti ir paimti "setName($x)", "getName()".
+   B) Pakeisti vardus ir atspausdinti
+    // uzduotis 3:
+    // 1) sukurti dvi klases: User, Admin
+    // 2) User klase turi kintamuosius : username, password, rights ir funkcija Login($name, $pass)
+    // 3) Admin klase  funkcijas:  changeUserName($nm),  changeRights($rgt)
