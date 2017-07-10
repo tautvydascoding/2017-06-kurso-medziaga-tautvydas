c407a9680ec16d997f09981ed021403973dc90d8
Tautvydas <arttgd@gmail.com>
2017-07-10 07:57:46 +0300

    14

--- uzduotys/uzduotis10/jQuery-uzduotis.js
+++ uzduotys/uzduotis10/jQuery-uzduotis.js
@@ -3,3 +3,4 @@
 // uzdeti "h1" elementui  css: spalva - raudona, fono spalva - juoda,  font dydis- 78, margin 200px 450px
 //   paspaudus ant paveiksliuko -   padidinti jo ploti 
 // uzduotis : pasletpi paveiksliuka tik ta kur paspaudi
+// uzduotis: uzvedus pele ant nuotraukos, ji turi tapti 50 % permatoma, nuvedus -  vel 100%
