804131e13e93af14e01d7e4be6238b8058f99b24
splin911 <paulius.pastauka@gmail.com>
2017-07-10 10:57:02 +0300

    zinute

--- uzduotys/uzduotis7/Paulius/MAMP/uzduotis7 frwmrk php validation is manto/js/main.js
+++ uzduotys/uzduotis7/Paulius/MAMP/uzduotis7 frwmrk php validation is manto/js/main.js
@@ -9,10 +9,15 @@ function tikrintiFormosLaukus(Event){
     x.preventDefault(); ////sustabdo submit veiksma. Ji padareme auksciau.
     
     vardas = document.querySelector("input['name=firstname']").value;
-    if(vardas.length < 3){
-        alert("koks cia vardas??")
-    }
+    
+    if(vardas.length > 3 && klausimas.length > 10 && elPastas.length > 5) {
+        console.log("Hey you");
+    } else {console.log("beat it");}
+    
     klausimas = document.querySelector("input['name=question']").value;
+    if (klausimas < 1) {
+    console.log("Hey you");
+}
     elPastas = document.querySelector("input['name=email']").value;
     
     console.log("vardas" + vardas);
