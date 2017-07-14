cf4fd991fbbfa4d2bcacfd3a2bbccadc3f8a3164
tautvydascoding <tautvydascoding@gmail.com>
2017-07-14 11:41:33 +0300

    18 paskiata

--- uzduotys/MAMP/uzduotis18/tautvydas/1-php-forma-i-db/save-article.php
+++ uzduotys/MAMP/uzduotis18/tautvydas/1-php-forma-i-db/save-article.php
@@ -1,10 +1,14 @@
 <?php
 
+
   echo "labas  <br /> <br /> <br />";
   //uzduotis: isvesti title ir contnet ivestus laukus
   echo "title: ". $_GET['article-title'] ."<br>";
   echo "turinuys:" . $_GET['article-content'] . " <br />";
   // print_r($_GET);
+
+
+  include_once('./db.php');
 
   // paimamae ka ivede vartotojas i forma
   $title = $_GET['article-title'] ;
