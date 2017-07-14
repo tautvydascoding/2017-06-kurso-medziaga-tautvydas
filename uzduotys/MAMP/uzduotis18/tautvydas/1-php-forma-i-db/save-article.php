52938be70012f152d49f77da710f70f502c62ef1
tautvydascoding <tautvydascoding@gmail.com>
2017-07-14 11:37:22 +0300

    18 paskiata

--- uzduotys/MAMP/uzduotis18/tautvydas/1-php-forma-i-db/save-article.php
+++ uzduotys/MAMP/uzduotis18/tautvydas/1-php-forma-i-db/save-article.php
@@ -1,5 +1,17 @@
 <?php
 
-  echo "labas";
+  echo "labas  <br /> <br /> <br />";
   //uzduotis: isvesti title ir contnet ivestus laukus
+  echo "title: ". $_GET['article-title'] ."<br>";
+  echo "turinuys:" . $_GET['article-content'] . " <br />";
+  // print_r($_GET);
+
+  // paimamae ka ivede vartotojas i forma
+  $title = $_GET['article-title'] ;
+  $content = $_GET['article-content'];
+  $user_ID = 2;
+  createArticle($title, $content, $user_ID);
+
+  echo "<h2>Jusu straipsnis issaugotas</h2>";
+
  ?>
