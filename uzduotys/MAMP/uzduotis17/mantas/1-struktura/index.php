ae4cd91618b0543bb000f275f1d8f17caad0647d
Mantasman <afixas@yahoo.com>
2017-07-14 12:39:32 +0300

    laravel

---
+++ uzduotys/MAMP/uzduotis17/mantas/1-struktura/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <html lang="lt">
+  <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+  <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css" />
+  <!-- main.css eina po visu kitu CSS failu !!!! -->
+  <link rel="stylesheet" type="text/css" href="css/main.css" />
+  <title>aa</title>
+</head>
+
+
+<body class="  ">
+
+
+    <div class="container-fluid backgroudn-img">
+
+<h1>ketvirtadienis</h1>
+<?php
+  include_once('./home-content.php');
+
+ ?>
+
+
+    </div> <!--  end container-fluid -->
+</body>
+
+</html>
