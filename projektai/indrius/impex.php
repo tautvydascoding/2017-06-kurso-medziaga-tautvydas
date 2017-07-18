db9c23b442eb2102b665726363086dae76591d1d
Derongalatis <deronbderon@gmail.com>
2017-07-18 16:51:36 +0300

    Projektas
    Projektas

---
+++ projektai/indrius/impex.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Naujas puslapis</title>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css" />
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+
+<body>
+  <div class="impex">
+
+
+  <?php include 'meniu2.php'; ?>
+  <div class="article">
+<br>
+  <h1>Apie įmonę</h1>
+
+          <p>UAB „IMP-EX Lietuva“ įkurta 2010 metais. Nuo pat įmonės įsikūrimo, teikiame kokybiškas pervežimų paslaugas Europoje, bei Pabaltijo šalyse. Įmonės darbuotojai kompetetingi negabaritinių krovinių pervežimams, krovinių su temperatūriniu režimu ar pakabinamų drabužių pervežimams. Pervežimus atliekame tiek nuosavu transportu, tiek pagal partnerystės sutartis samdytu transportu. Turime kelias transporto priemones iki 3,5t, kuriomis galime vežti skubius užsakymus pagal kliento poreikius. Šios transporto priemonės pritaikytos vežti ir pakabinamus rūbus. Mūsų įmonė dar jauna, tačiau joje dirba didelę transporto ir logistikos patirtį turinti komanda. Pagrindiniai maršrutai: Vokietija, Olandija, Belgija, Didžioji Britanija, Danija, Švedija, Italija, Ispanija.
+  </p>
+        <h1>Misija</h1>
+        <p>Mūsų misija. Kodėl mes esame?
+
+Teikti klientams optimaliausias transporto logistikos paslaugas tarptautinėse ir vietinėse rinkose.
+
+Tai reiškia, kad mes: išklausome klientus, dalinamės su jais turimomis žiniomis ir stengiamės visapusiškai patenkinti jų poreikius bei lūkesčius, sukuriame savo klientams individualių pasiūlymų ir paslaugų paketą, kuris užtikrina maksimalų patikimumą.
+
+</p>
+       <h1>Vizija</h1>
+       <p>Mūsų vizija. Kuo norime būti?
+
+Būti patikimu partneriu siekiantiems aukščiausių rezultatų ir tapti vienu iš lyderių Lietuvos transporto ir logistikos paslaugų rinkoje.
+
+Tai reiškia, kad mes dirbame, siekdami pelnyti klientų pasitikėjimą kaip jų partneris kiekvieną dieną, o šis pasitikėjimas mus veda lyderystės link.
+
+</p>
+        <h1>Klientai</h1>
+         <p>Mums svarbus kiekvienas klientas, todėl mūsų paslaugos domina ne tik tarptautines įmones, bet ir naujai besikuriančias bendroves ar vidutinio dydžio įmones. Per pirmuosius du gyvavimo metus UAB „IMP-EX LIETUVA“ paslaugos buvo teikiamos, ne tik Lietuvos, bet ir Anglijos, Baltarusijos, Čekijos, Estijos, Danijos, Ispanijos, Italijos, Latvijos, Lenkijos, Rusijos, Šveicarijos, Ukrainos, Vokietijos gamintojams, bei logistikos ar prekybos įmonėms.</p>
+
+       </div>
+       </div>
+
+
+
+</body>
+
+
+</html>
